850354da73aaed86a61ef7a2f58b7bd88af22fa8
dgobardja <demetriuselchinou.gobardja@student.unimelb.edu.au>
2023-12-07 16:39:06 +0700

    node based tournament bracket
    I'm pretty sure its really buggy but wont know until we test it properly

--- GameManager.py
+++ GameManager.py
@@ -1,8 +1,7 @@
-# Game manager
 import random
-import Player1
-import Player2
 from test import *
+import importlib
+
 #game settings
 timeLimit = 60
 movesPerSecond =2
@@ -10,21 +9,37 @@ movesPerSecond =2
 #move how many unit per tick
 gravity = 2
 
+#direction constants
+
+GORIGHT = 1
+GOLEFT = -1
+
 #player variables
 
+def setupGame(path1, path2):
+    
+    p1Import = importlib.import_module("Submissions." + path1)
+    p2Import = importlib.import_module("Submissions." + path2)
+    player1 = p1Import.Player_Controller(1,0,50,GORIGHT)
+    player2 = p2Import.Player_Controller(4,0,50,GOLEFT)
+    return player1,player2
 
-player1 = Player1.Player_Controller(1,0,50)
-player2 = Player1.Player_Controller(4,0,50)
 
 
-def move(player, action):
+def move(player, enemy, action):
     if (action[0] == "move"):
-        if validMove(action[1]):
+        if validMove(action[1], player, enemy) and not player.midair:
             player.moves.append(action)
-            player.xCoord += action[1][0]
+            player.xCoord += player.direction * action[1][0]
             player.yCoord += action[1][1]
-        else:
-            Exception
+            if player.yCoord > 0:
+                player.midair = True
+        elif player.midair:
+            player.yCoord -= 1
+            if player.yCoord == 0:
+                player.midair = False
+        else:    
+            raise Exception("Invalid movement direction")
 
 def block(player, action):
     if (action[0] == "block"):
@@ -36,31 +51,68 @@ def attack(player,target, action):
     if (action[0] == "attack"):
         player.moves.append(action)
         #TODO attack variations such as heavy and light
+
         # check if attack lands
-        if (abs(player.xCoord-target.xCoord) == 1 and player.yCoord == target.yCoord):
+        damage = action[1]
+        atk_range = action[2]
+        blockable = action[3]
+        
+        # This is fine if we only allow horizontal attacks
+        if (abs(player.xCoord-target.xCoord) == atk_range and player.yCoord == target.yCoord):
             #check for blocks
-            if(target.blocking):
+            if(target.blocking and blockable):
                 #parry if block is frame perfect
-                if target.moves[-2] != "block":
+                if target.moves[-1] != "block":
                     player.stun = 2
             else:
-                target.hp -=5
+                target.hp -= damage
+                
 
 
-def startGame():
-    for tick in range(5):
+
+def startGame(path1, path2):
+    print(path1,path2)
+    if not isinstance(path1, str) and isinstance(path2,str):
+        return path2
+    if isinstance(path1, str) and not isinstance(path2,str):
+        return path1
+    if not isinstance(path1, str) and not isinstance(path2,str):
+        return None
+    player1, player2 = setupGame(path1,path2)
+
+
+    for tick in range(timeLimit *movesPerSecond):
+        
+        # print(f"P1 : {player1.xCoord, player1.yCoord}")
+        # print(f"P2 : {player2.xCoord, player2.yCoord}")
+
+        #flips orientation if player jumps over each other
+        if flip_orientation(player1, player2):
+            player1.direction = GOLEFT
+            player2.direction = GORIGHT
+        else:
+            player1.direction = GORIGHT
+            player2.direction = GOLEFT
+            
         act1 = player1.action()
         act2 = player2.action()
-
-        move(player1, act1)
-        move(player2, act2)
-
-        block(player1, act1)
-        block(player2, act2)
-
-        attack(player1, player2, act1)
-        attack(player2, player1, act2)
+        
+        if not player1.stun:
+            move(player1, player2, act1)
+            block(player1, act1)
+            attack(player1, player2, act1)
+            player1.moveNum += 1
+        else:
+            player1.stun -= 1
             
-
-
-
+        if not player2.stun:
+            move(player2, player1, act2)
+            block(player2, act2)
+            attack(player2, player1, act2)
+            player2.moveNum += 1
+        else:
+            player2.stun -= 1
+    if player1.hp == player2.hp:
+        print('match won by: ', path1)
+        return path1
+    return max(player1.hp, player2.hp)
---
+++ Tournament.py
@@ -0,0 +1,66 @@
+import os
+import random
+from GameManager import startGame
+
+dir_path = os.path.join(os.getcwd(), "Submissions")
+player = os.listdir(dir_path)
+
+clean_player = []
+for i in player:
+    if i[-3:] == ".py":
+        clean_player.append(i[:-3])
+print(clean_player)
+
+
+
+class TournamentNode:
+    def __init__(self, player=None, left=None, right=None):
+        self.player = player
+        self.left = left
+        self.right = right
+
+def buildBracket(players):
+    if not players:
+        return None
+
+    if len(players) == 1:
+        return TournamentNode(player=players[0])
+
+    middle = len(players) // 2
+    left_child = buildBracket(players[:middle])
+    right_child = buildBracket(players[middle:])
+
+    return TournamentNode(left=left_child, right=right_child)
+
+def printBracket(node, depth=0):
+    if node:
+        print("  |" * depth, node.player)
+        printBracket(node.left, depth + 1)
+        printBracket(node.right, depth + 1)
+    
+
+def findWinner(node):
+    if not node:
+        return None
+
+    if node.player:
+        return node.player
+    printBracket(node)
+    print("\n")
+    left_winner = findWinner(node.left)
+    right_winner = findWinner(node.right)
+
+    return startGame(left_winner, right_winner)
+
+def simulateTournament(players_list):
+    root = buildBracket(players_list)
+    print("Initial Bracket:")
+    # a bunch of nones are cuz the winners arent listed
+    # the branch is printed sideways in a very weird :P
+    printBracket(root)
+    winner = findWinner(root)
+    print("\nThe winner is:", winner)
+
+
+
+simulateTournament(clean_player)
