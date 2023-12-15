77ca92d6d8d2dfdc0119dc64fcbd47b0f33c975e
Kenneth Lim <kenneth.lim456@gmail.com>
2023-12-15 22:51:27 +0700

    Fixed bugs and updated falling logic
    Added priority such that movement and blocks come out before attacks and skills
    Updated falling logic
    Fixed a bug that allowed dashes to move outside the game box( xCoord < 0 or   xCoord > 30)

--- Submissions/Player1.py
+++ Submissions/Player1.py
@@ -1,14 +1,5 @@
-<<<<<<< Updated upstream
-class Player_Controller:
-    def __init__(self, xCoord, yCoord, HP, direction):
-        self.primarySkill =""
-        self.secondarySkill = ""
-        self.xCoord = xCoord
-        self.yCoord = yCoord
-        self.moves = []
-=======
+
 from Skills import AttackSkill, BlockSkill
-# use as a template for player1 and player2
 class Player_Controller:
     def __init__(self, xCoord, yCoord, HP, direction):
         self.primarySkill ="dash_attack"
@@ -19,7 +10,6 @@ class Player_Controller:
         self.heavyAtk = AttackSkill(2, 4, 10, 2, True, 1, 1)
         self.block = BlockSkill(0, 0, 15, 2)
         
->>>>>>> Stashed changes
         self.blocking = False
         self.hp = HP
         self.jumpHeight = 2
@@ -35,13 +25,9 @@ class Player_Controller:
         self.moves = []
         #for testing
         #player scripts will append moves to this
-<<<<<<< Updated upstream
-        self.moveList = [("move", (1,1)), ("move",(1,1)), ("move", (-1,0))]
-=======
         self.moveList = [("dash_attack", None), ("move", (1,1)), ("move",(1,1)), ("move", (-1,0)), ("attack", "light"), ("attack", "heavy")]
->>>>>>> Stashed changes
     def action(self):
         if self.moveNum < len(self.moveList):
-            print(f"{self.moveNum} , {self.moveList[self.moveNum]}")
+            # print(f"{self.moveNum} , {self.moveList[self.moveNum]}")
             return self.moveList[self.moveNum]
         return ("NoMove")
---
+++ playerActions.py
@@ -0,0 +1,92 @@
+from test import validMove
+def move(player, enemy, action):
+    if (action[0] == "move"):
+        if validMove(action[1], player, enemy) and not player.midair:
+            player.blocking = False
+            player.block.regenShield()
+            player.moves.append(action)
+            player.xCoord += player.direction * action[1][0]
+            player.yCoord += action[1][1]
+            if player.yCoord > 0:
+                player.midair = True
+        else:    
+            print("Invalid movement")
+    return None, None
+
+def block(player, target, action):
+    if (action[0] == "block"):
+        player.moves.append(action)
+        player.blocking = True
+    return None, None
+
+#returns the action if not on cooldown or mid-startup.
+# if on cd, return current cd, or -1 if mid startup
+def fetchAttack(player, attackType):
+    if attackType == "light":
+        return player.lightAtk.activateSkill()
+    elif attackType == "heavy":
+        return player.heavyAtk.activateSkill()
+    else:
+        raise Exception("Invalid attack type!")
+
+
+def attack(player,target, action):
+    knockback = stun = 0
+    if (action[0] == "attack"):
+        player.blocking = False
+        player.block.regenShield() 
+
+        # 2 types of attack, light and heavy
+        # action should be like ("attack", "light/heavy")
+        attack = fetchAttack(player, action[1])
+        
+        # no action if attack is on cooldown or previous attack is still in startup
+        if isinstance(attack, int):
+            return 0, 0
+        
+        player.moves.append(action)
+
+        print(attack)
+        # fetch attack data
+        damage = attack[1]
+        atk_range = attack[2]
+        blockable = attack[3]
+        knockback = attack[4]
+        stun = attack[5]
+        
+        # This is fine if we only allow horizontal attacks
+        if (abs(player.xCoord-target.xCoord) == atk_range and player.yCoord == target.yCoord):
+            # can be changed later : no knockback if block or stunned
+            if target.blocking or target.stun:
+                knockback = 0
+            # if target is blocking
+            if(target.blocking and blockable):
+                #parry if block is frame perfect: the target blocks as attack comes out
+                if target.moves[-1] == "block" and target.moves[-2] != "block":
+                    player.stun = 2
+                elif target.blocking:
+                    target.stun += target.block.shieldDmg(damage)
+                    pass
+            else:
+                target.hp -= damage
+    return knockback, stun
+
+# dashes towards target, deals damage along the way
+#TODO make this a method in skills.py
+def dash_atk(player, target, action):
+    if not player.primarySkill == "dash_attack":
+        print("Player primary skill is not dash_attack - invalid move")
+        return 0, 0
+    
+    if (action[0] == "dash_attack"):
+        print("did dash atk")
+        player.xCoord += 4 * player.direction
+
+        #blockable = True
+        #atk_range = 1
+        #damage = 5
+        #knockback = stun =0
+
+        return attack(player, target, ("attack", "heavy"))
+
+valid_actions = {"attack": attack, "block": block, "move": move, "dash_attack": dash_atk}
