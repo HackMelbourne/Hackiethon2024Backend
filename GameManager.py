2d89df9348beea326cacfa96b574ad01cdcf8e00
Kenneth Lim <kenneth.lim456@gmail.com>
2024-02-10 14:06:11 +1100

    cleaned up some code, moved turn based updates to separate file

--- GameManager.py
+++ GameManager.py
@@ -1,20 +1,16 @@
-import random
-from test import *
+import test
 import importlib
-from playerActions import *
+from playerActions import defense_actions, attack_actions, projectile_actions
 from Skills import *
 from projectiles import *
 import json
 import os
+from turnUpdates import *
 #game settings
 timeLimit = 6
 movesPerSecond = 1
 
-# number of y-units to move when falling
-gravity = 1
-
 #direction constants
-
 GORIGHT = 1
 GOLEFT = -1
 
@@ -31,56 +27,11 @@ def setupGame():
 #------------------Adding to player1 and player2 move scripts for test----
 def setMoves(player1, player2):    
     p1movelist = None,
-    p2movelist = ("lasso", None), ("skill_cancel",), ("move", (1,1)), ("move", (1,1)), ("move", (1,1)), ("move", (1,1)), ("move", (1,1))
+    p2movelist = ("lasso", None), ("skill_cancel",), ("move", (1,1)), 
+    ("move", (1,1)), ("move", (1,1)), ("move", (1,1)), ("move", (1,1))
     
     player1._inputs += p1movelist
     player2._inputs += p2movelist          
-
-def updateCooldown(player):
-    player._lightAtk.reduceCd(1)
-    player._heavyAtk.reduceCd(1)
-    player._primarySkill.reduceCd(1)
-    player._secondarySkill.reduceCd(1)
-    
-# updates current position of player if they are midair or started jumping
-def updateMidair(player):
-    # check if player should be falling
-    player._falling = (player._yCoord >= player._jumpHeight)
-    # not yet at apex of jump
-    if player._midair:
-        if player._falling: 
-            # specifically to check for diagonal jumps, ensure jump arc
-            # like _ - - _
-            if player._moves[-2] != ("move",(1,1)):
-                player._yCoord -= gravity
-        else:
-            player._yCoord += 1
-        player._xCoord += player._velocity
-
-    # player has landed, reset midair attributes
-    if player._yCoord <= 0 and player._falling: 
-        player._midair = player._falling = False
-        player._velocity = 0
-        player._jumpheight = 2
-
-def playerToJson(player, jsonDict):
-    jsonDict['hp'].append(player._hp)
-    jsonDict['xCoord'].append(player._xCoord)
-    jsonDict['yCoord'].append(player._yCoord)
-    jsonDict['state'].append(player._moves[-1][0])
-    jsonDict['stun'].append(player._stun)
-    jsonDict['midair'].append(player._midair)
-    jsonDict['falling'].append(player._falling)
-
-def projectileToJson(projectile, jsonDict, travelling):
-    if travelling:
-        jsonDict['ProjectileType'] = projectile.type
-        jsonDict['projXCoord'].append(projectile.xCoord)
-        jsonDict['projYCoord'].append(projectile.yCoord)
-    else:
-        jsonDict['projXCoord'].append(None)
-        jsonDict['projYCoord'].append(None)    
-                
 
 def performActions(player1, player2, act1, act2, stun1, stun2, projectiles):
     knock1 = knock2 = 0
@@ -100,11 +51,7 @@ def performActions(player1, player2, act1, act2, stun1, stun2, projectiles):
     # and then add it to attack_actions or defense_actions (inside playerActions)
 
     # works under assumption of only 1 action per turn
-
-    # for debug
-    # if (act1 != "NoMove"):
-    # print(act1, act2)
-    
+        
     # movement and defensive actions take priority then attacks and skills 
     if act1 in ("NoMove", ("NoMove", None), None):
         player1._moves.append(("NoMove", None))
@@ -130,107 +77,7 @@ def performActions(player1, player2, act1, act2, stun1, stun2, projectiles):
     player2._moveNum += 1
     
     return knock1, stun1, knock2, stun2
-                
-def proj_knockback(proj, player):
-    if proj.xCoord < player._xCoord:
-        return -1
-    return 1
-                        
-
-def projectile_move(projectiles, knock1, stun1, knock2, stun2, player1, player2,
-                    p1_dict, p2_dict):
-    for projectileNum in range(len(projectiles)):
-        
-        proj_info = projectiles[projectileNum]
-        proj_obj = proj_info["projectile"]
-        
-        if proj_obj.player._id == 1:
-            proj_json_dict = p1_dict
-        else:
-            proj_json_dict = p2_dict
-        
-        # if exists, then travel and log
-        proj_obj.travel()
-        
-        # first check if the projectile already travelled its range or offscreen
-        if proj_obj.size == (0,0):
-            # remove projectile from array
-            projectiles.pop(projectileNum)
-            projectileToJson(proj_obj, proj_json_dict, False)
-            continue
-        
-        # also check if projectile can be and has been cancelled
-        if proj_info["self_stun"] and proj_obj.player._moves[-1][0] == "skill_cancel":
-            projectiles.pop(projectileNum)
-            projectileToJson(proj_obj, proj_json_dict, False)
-            continue   
-           
-        projectileToJson(proj_obj, proj_json_dict, True)
-        print(f"PROJ {proj_obj.xCoord, proj_obj.yCoord}")
-        
-        # check for projectiles colliding with each other
-        for nextProjNum in range(len(projectiles)):
-            nextproj_obj = projectiles[nextProjNum]["projectile"]
-            if (nextProjNum != projectileNum and 
-                proj_obj.checkProjCollision(nextproj_obj)):
-                    projectiles.pop(projectileNum)
-                    projectiles.pop(nextproj_obj)
-                    break
-        
-        # list of ids of projectiles currently on screen
-        projectile_ids = [projectile_obj["projectile"].id for projectile_obj in projectiles]
-        # check if this projectile still exists
-        if proj_obj.id in projectile_ids:
-            # get projectile info and initialise
-            proj_info = projectiles[projectileNum]
-            proj_knock1 = proj_knock2 = proj_stun1 = proj_stun2 = 0
-            # collision checks and attack checks
-            if proj_obj.checkCollision(player1):
-                # if explosive, the knockback depends on where the enemy was
-                knockback = proj_info["knockback"] * proj_knockback(proj_obj, player1)
-                proj_knock2, proj_stun2 = attackHit(proj_obj, player1,
-                                                proj_info["damage"],
-                                                proj_obj.size[0],
-                                                proj_obj.size[1],
-                                                proj_info["blockable"],
-                                                knockback,
-                                                proj_info["stun"])
-            if proj_obj.checkCollision(player2):
-                knockback = proj_info["knockback"] * proj_knockback(proj_obj, player2)
-                proj_knock1, proj_stun1 = attackHit(proj_obj, player2,
-                                                proj_info["damage"],
-                                                proj_obj.size[0],
-                                                proj_obj.size[1],
-                                                proj_info["blockable"],
-                                                knockback,
-                                                proj_info["stun"])
-            # if attack and projectile hits target at same time, use
-            # total knockback and highest stun
-            
-            # this is if the projectile explodes
-            if proj_obj.trait == "explode":
-                proj_obj.size = (0,0)
-            
-                
-                
-            knock1 += proj_knock1
-            stun1 = max(stun1, proj_stun1)
-            knock2 += proj_knock2
-            stun2 = max(stun2, proj_stun2)
-            
-            # for explosive projectiles
-                            
-            # then pop the projectile if it hit or expires, else continue travel
-            if proj_knock1 or proj_knock2 or proj_obj.size == (0,0):
-                projectileToJson(proj_obj, proj_json_dict, False)
-                projectiles.pop(projectileNum)
-                # then unstun caster if the projectile skill has self stun
-                if proj_info["self_stun"]:
-                    proj_obj.player._skill_state = False
-
-                    
-    return projectiles, knock1, stun1, knock2, stun2    
-
+                                        
 def startGame(path1, path2):
     if not isinstance(path1, str) and isinstance(path2,str):
         return path2
@@ -241,8 +88,7 @@ def startGame(path1, path2):
     player1, player2 = setupGame()
 
     stun1 = stun2 = 0
-    
-    #TODO uncomment to set moves for both players
+
     setMoves(player1, player2)
 
     #TODO dont hard code path use the player names and use os for current path
@@ -280,11 +126,10 @@ def startGame(path1, path2):
         'projYCoord':[]
     }
     projectiles = []
-    
-    
+  
     for tick in range(timeLimit *movesPerSecond):
         #flips orientation if player jumps over each other
-        if flip_orientation(player1, player2):
+        if test.flip_orientation(player1, player2):
             player1.direction = GOLEFT
             player2.direction = GORIGHT
         else:
@@ -296,8 +141,8 @@ def startGame(path1, path2):
         act1 = player1._action()
         act2 = player2._action()
         
-        playerInfo(player1, path1, act1)
-        playerInfo(player2, path2, act2)
+        test.playerInfo(player1, path1, act1)
+        test.playerInfo(player2, path2, act2)
         
         knock1, stun1, knock2, stun2 = performActions(player1, player2, 
                                                       act1, act2, stun1, stun2, 
@@ -321,21 +166,17 @@ def startGame(path1, path2):
         updateMidair(player2)
 
         # correct player positions if off screen/under ground
-        correctPos(player1)
-        correctPos(player2)
+        test.correctPos(player1)
+        test.correctPos(player2)
         
-        correctOverlap(player1, player2, knock1, knock2)
+        test.correctOverlap(player1, player2, knock1, knock2)
             
         updateCooldown(player1)
         updateCooldown(player2)
         
-        #TODO update current startup every tick 
-
-        print(player2._moves)
         playerToJson(player1, p1_json_dict)
         playerToJson(player2,p2_json_dict)
-    #print(p1_json_dict)
-    #TODO uncomment to dump data to json files
+
     json.dump(p1_json_dict, player1_json)
     json.dump(p2_json_dict, player2_json)
     
@@ -344,9 +185,16 @@ def startGame(path1, path2):
 
     print(p1_json_dict)
     print(p2_json_dict)
-    if player1._hp == player2._hp:
+    
+    if player1._hp > player2._hp:
         print('match won by: ', path1)
         return path1
-    return max(player1._hp, player2._hp)
+    elif player1._hp < player2._hp:
+        print('match won by: ', path2)
+        return path2
+    else:
+        print('Tie!')
+        return None
 
-startGame("a", "b")
+if __name__ == "__main__":
+    startGame("PLAYER 1", "PLAYER 2")
